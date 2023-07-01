008dbde651826e1785408ff250921ea51d228fca
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2023-07-02 02:16:54 +0530

    Courotine job - return type of launch

--- app/src/main/java/com/example/coroutinebasicfunction/MainActivity.kt
+++ app/src/main/java/com/example/coroutinebasicfunction/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -18,29 +19,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         coroutineText = findViewById(R.id.coroutine_text)
 
-        Log.d(Tag,"before coroutine")
+        val job = GlobalScope.launch(Dispatchers.Default) {
 
-        runBlocking {
-            Log.e(Tag,"cor start: ${Thread.currentThread().name}")
-
-            //we can switch the thread inside the runBlocking with the help of launch() function
-
-            launch(Dispatchers.IO){
-                delay(3000L)
-                Log.e(Tag,"finished coroutine 1 launch scope: ${Thread.currentThread().name}")
+            repeat(4){  //repeat is a kotlin inline function
+                Log.e(Tag,"job is getting repeated")
+                delay(1000L)
             }
-
-            launch(Dispatchers.IO){
-                delay(4000L)
-                Log.e(Tag,"finished coroutine 2")
-            }
-
-            Log.e(Tag,"start of running block: ${Thread.currentThread().name}")
-            delay(3000L)
-            Log.e(Tag,"end of running block")
         }
 
-        Log.e(Tag,"after running block")
+        runBlocking {
+            job.join() //job.join -- suspend function ,
+                       // so it is called inside coroutine scope here , we might called it from other suspend function.
+            Log.e(Tag,"repeation job complete")
+        }
 
     }
 
