22f9b2d65b21a96d715e1bb451a009959f53126d
PrernaSurbhi5 <Prerna.Surbhi@tsb.co.uk>
2023-07-12 17:44:03 +0530

    usingWithOutOrNull() - NetWorkRequestTimeOut

--- app/src/main/java/com/example/coroutinebasicfunction/NetWorkRequestTimeOutModel.kt
+++ app/src/main/java/com/example/coroutinebasicfunction/NetWorkRequestTimeOutModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
 import java.lang.Exception
 
 class NetWorkRequestTimeOutModel:ViewModel() {
@@ -19,6 +20,24 @@ class NetWorkRequestTimeOutModel:ViewModel() {
               //  uiState.value = UiState.error("network request timeout")
             }catch(exception:Exception){
                 //  uiState.value = UiState.error("network request falied")
+            }
+
+        }
+    }
+
+    private fun usingWithOutOrNull(timeOut:Long){
+        viewModelScope.launch {
+            try{
+                val recentAndroidVersions = withTimeoutOrNull(timeOut){
+                    //  mockApi.getRecentAndroidVersions();
+                }
+                if(recentAndroidVersions != null){
+                      //  uiState.value = UiState.Sucess(recentAndroidVersions)
+                }else{
+                    //  uiState.value = UiState.Error("network request timeout")
+                }
+            }catch(exception:Exception){
+                //  uiState.value = UiState.Error("network request falied")
             }
 
         }
